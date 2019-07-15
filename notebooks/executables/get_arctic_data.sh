aacb46eabbab246ad86a63ead0917adc7adb6fbc
kiri93 <mk.youssef@hotmail.com>
2019-07-15 11:54:41 +0200

    upload notebooks

---
+++ notebooks/executables/get_arctic_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd frames && { curl -LO "https://svs.gsfc.nasa.gov/vis/a000000/a004500/a004510/frames/1920x1080_16x9_60p/WeeklyIceAge/weeklySeaIceAge2.[0000-4944].tif" ;}
---
+++ notebooks/executables/unzip_gene_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+unzip data/raw_data/genes/circadiaNET_correlation_matrices.zip -d data/raw_data/genes
