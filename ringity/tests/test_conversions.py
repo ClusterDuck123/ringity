77f5f8a4de6ff70d73f2258e431f8521a0435e33
ClusterDuck123 <mk.youssef@hotmail.com>
2019-11-08 00:40:44 +0100

    cleanup

--- ringity/tests/test_conversions.py
+++ ringity/tests/test_conversions.py
@@ -18,27 +18,26 @@ import numpy as np
 import networkx as nx
 
 class TestConversions(unittest.TestCase):
-    
+
     def test_ddict2dict2ddict_unweighted(self):
         E = nx.erdos_renyi_graph(100,0.17)
         d = dict(E.edges)
-        dd = ringity.routines.dict2ddict(d)
-        ddd = ringity.routines.ddict2dict(dd)
-        dddd = ringity.routines.dict2ddict(ddd)
+        dd = ringity.methods.dict2ddict(d)
+        ddd = ringity.methods.ddict2dict(dd)
+        dddd = ringity.methods.dict2ddict(ddd)
         self.assertEqual(dd, dddd)
-        
+
     def test_ddict2dict2ddict_weighted(self):
         E = nx.erdos_renyi_graph(100,0.17)
         for (u, v) in E.edges():
             E[u][v]['weight'] = np.random.uniform(-1,1)
-            
+
         d = dict(E.edges)
-        dd = ringity.routines.dict2ddict(d)
-        ddd = ringity.routines.ddict2dict(dd)
-        dddd = ringity.routines.dict2ddict(ddd)
+        dd = ringity.methods.dict2ddict(d)
+        ddd = ringity.methods.ddict2dict(dd)
+        dddd = ringity.methods.dict2ddict(ddd)
         self.assertEqual(dd, dddd)
-        
-        
+
+
 if __name__ == '__main__':
     unittest.main()
-        
