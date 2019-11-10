ae6593870a8e0bc163ca00c821166c31507c03a7
ClusterDuck123 <mk.youssef@hotmail.com>
2019-11-10 19:37:03 +0100

    fixed version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as f:
 
 setuptools.setup(
     name="ringity",
-    version="0.0a4",
+    version="0.0a5",
     author="Markus K. Youssef",
     author_email="mk.youssef@hotmail.com",
     description="ringity package",
