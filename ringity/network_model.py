7e376d57f9dc96f84dac1609131dfe1c19469cd1
ClusterDuck123 <mk.youssef@hotmail.com>
2020-07-06 16:59:09 +0200

    new model

--- ringity/network_model.py
+++ ringity/network_model.py
@@ -1,4 +1,4 @@
-from ringity.distribution_functions import get_mu, mueta_to_alphabeta
+from ringity.distribution_functions import mean_similarity, cdf_similarity
 from scipy.spatial.distance import pdist, squareform
 from numpy import pi as PI
 
@@ -10,8 +10,8 @@ import networkx as nx
 #  -------------------------------  PREPARATION -----------------------------
 # =============================================================================
 def geodesic_distances(thetas):
-    abs_dist = pdist(thetas.reshape(-1,1))
-    return np.where(abs_dist<PI, abs_dist, 2*PI-abs_dist)
+    abs_dists = pdist(thetas.reshape(-1,1))
+    return np.where(abs_dists<PI, abs_dists, 2*PI-abs_dists)
 
 
 def overlap(dist, a):
@@ -19,27 +19,35 @@ def overlap(dist, a):
     Calculates the overlap of two boxes of length 2*pi*a on the circle for a
     given distance dist (measured from center to center).
     """
-    x1 = np.where(2*PI*a-dist     > 0, 2*PI*a-dist    , 0)
-    # for large box sizes (a>0) there is a second overlap
-    x2 = np.where(dist-2*PI*(1-a) > 0, dist-2*PI*(1-a), 0)
-    return x1 + x2
 
-
-def connection_function(rho, eta, a, kappa):
-    mu = get_mu(rho=rho, eta=eta, kappa=kappa, a=a)
-    if   eta == 0:
-        return lambda t : np.where(t < mu, 0., 1.)
-    elif eta == 1:
-        return lambda t : rho*np.ones_like(t)
+    x1 = (2*PI*a-dist).clip(0)
+    if a <= 0.5:
+        return x1
+    # for box sizesw with a>0 there is a second overlap
     else:
-        alpha, beta = mueta_to_alphabeta(mu, eta)
-        return lambda t : scipy.stats.beta.cdf(t, a=alpha, b=beta)
+        x2 = (dist-2*PI*(1-a)).clip(0)
+        return x1 + x2
+
+
+def slope(rho, kappa, a):
+    mu_S = mean_similarity(kappa,a)
+    if rho <= mu_S:
+        return rho/mu_S
+    else:
+        const = 1/np.sinh(PI*kappa)
+        def integral(k):
+            term1 = np.sinh((1 + 2*a*(1/k-1))*PI*kappa)
+            term2 = (k*np.sinh((a*PI*kappa)/k)*np.sinh(((a+k-2*a*k)*PI*kappa)/k))/(a*PI*kappa)
+            return term1-term2
+        return scipy.optimize.newton(
+            func = lambda k: const*integral(k) + (1-cdf_similarity(1/k, kappa, a)) - rho,
+            x0 = rho/mu_S)
 
 # =============================================================================
 #  ------------------------------  NETWORK MODEL ----------------------------
 # =============================================================================
 
-def network_model_samples(N, rho, eta=0, a=0.5, kappa=0):
+def weighted_network_model(N, rho, a=0.5, kappa=0):
     """
     Returns samples of the Network model as described in [1]
     The outputs are samples of the
@@ -53,41 +61,35 @@ def network_model_samples(N, rho, eta=0, a=0.5, kappa=0):
     # just maiking sure no one tries to be funny...
     assert 0 <= kappa
     assert 0 <   a  <= 0.5
-    assert 0 <= eta <= 1
     assert 0 <= rho <= 1
+    assert 1-rho > np.sinh((PI-2*a*PI)*kappa) / np.sinh(PI*kappa)
 
-    H = connection_function(rho=rho, eta=eta, kappa=kappa, a=a)
+    posis = np.random.exponential(scale=1/kappa, size=N) % (2*PI)
+    dists = geodesic_distances(posis)
+    simis = overlap(dists, a)/(2*PI*a)
+    probs = simis*slope(rho, kappa, a)
 
-    positions = np.random.vonmises(mu=0, kappa=kappa, size=N)
-
-    abs_dist = scipy.spatial.distance.pdist(positions.reshape(N,1))
-    sqNDM = np.where(abs_dist<PI, abs_dist, 2*PI-abs_dist)
-    sqNSM = overlap(sqNDM, a)/(2*PI*a)
-    sqNPM = H(sqNSM)
-
-    return (positions,) + tuple(map(squareform, [sqNDM, sqNSM, sqNPM]))
+    return probs
 
 
-def network_model(N, rho, eta=0, kappa=0, a=0.5):
+def network_model(N, rho, kappa, a=0.5):
     """
     Network model as described in [1]. The output is the (empirical) positions
     of the nodes placed on the circle according to a von Mises distribution,
     followed by a tripple consisting of the (empirical) distribution of the
     pairwise distances, similarities and connection probabilities respectively.
     """
-    positions, NDM, NSM, NPM = network_model_samples(N = N,
-                                                     rho = rho,
-                                                     eta = eta,
-                                                     kappa = kappa,
-                                                     a = a)
-    R = squareform(np.random.uniform(size=round(N*(N-1)/2)))
-    A = np.where(NPM>R, 1, 0)
+    probs = weighted_network_model(N = N,
+                                   rho = rho,
+                                   kappa = kappa,
+                                   a = a)
+    rands = np.random.uniform(size=round(N*(N-1)/2))
+    A = squareform(np.where(probs>rands, 1, 0))
     G = nx.from_numpy_array(A)
     return G
 
 # =============================================================================
 #  ------------------------------ REFERENCES ---------------------------------
 # =============================================================================
-"""
-[1] Not published yet.
-"""
+
+# [1] Not published yet.
