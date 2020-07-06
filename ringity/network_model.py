01091984cae65aa0a38c6af83f5808fbdd474a9c
ClusterDuck123 <mk.youssef@hotmail.com>
2020-07-06 20:36:53 +0200

    new model with edge cases

--- ringity/network_model.py
+++ ringity/network_model.py
@@ -28,14 +28,13 @@ def overlap(dist, a):
         x2 = (dist-2*PI*(1-a)).clip(0)
         return x1 + x2
 
-
 def slope(rho, kappa, a):
     mu_S = mean_similarity(kappa,a)
     if rho <= mu_S:
         return rho/mu_S
     else:
         const = 1/np.sinh(PI*kappa)
-        def integral(k):
+        def integral(k): # This can probably be further simplified
             term1 = np.sinh((1 + 2*a*(1/k-1))*PI*kappa)
             term2 = (k*np.sinh((a*PI*kappa)/k)*np.sinh(((a+k-2*a*k)*PI*kappa)/k))/(a*PI*kappa)
             return term1-term2
@@ -47,46 +46,79 @@ def slope(rho, kappa, a):
 #  ------------------------------  NETWORK MODEL ----------------------------
 # =============================================================================
 
-def weighted_network_model(N, rho, a=0.5, kappa=0):
+def weighted_network_model(N, rho, beta, a=0.5, return_positions=False):
     """
     Returns samples of the Network model as described in [1]
     The outputs are samples of the
-     - positions of the nodes placed on the circle according to a von
-       Mises distribution,
+     - positions of the nodes placed on the circle according to a
+       (wrapped) exponential distribution,
      - their pairwise distances
      - their similarities, given an 'activity window' of size a
-     - their connection probabilities, given the parameters eta and rho.
+     - their connection probabilities, given the expected density rho.
     """
 
     # just maiking sure no one tries to be funny...
-    assert 0 <= kappa
-    assert 0 <   a  <= 0.5
-    assert 0 <= rho <= 1
-    assert 1-rho > np.sinh((PI-2*a*PI)*kappa) / np.sinh(PI*kappa)
 
-    posis = np.random.exponential(scale=1/kappa, size=N) % (2*PI)
-    dists = geodesic_distances(posis)
-    simis = overlap(dists, a)/(2*PI*a)
-    probs = simis*slope(rho, kappa, a)
+    assert 0 <= beta <= 1
+    assert 0 <   a   <= 0.5
+    assert 0 <= rho  <= 1
 
-    return probs
+    if beta == 0:
+        posis = np.zeros(N)
+        probs = rho*np.ones(int(N*(N-1)/2))
+    elif beta == 1:
+        assert rho < 2*a, "Please increase `a` or decrease `rho`!"
+        posis = np.random.uniform(0,2*PI, size=N)
+        dists = geodesic_distances(posis)
+        simis = overlap(dists, a)/(2*PI*a)
+
+        if rho <= a:
+            k = rho/a
+        else:
+            k = a/(2*a-rho)
+        probs = (simis*k).clip(0,1)
+    else:
+        kappa = np.tan(PI*(1-beta)/2)
+        assert rho < 1-np.sinh((PI-2*a*PI)*kappa) / np.sinh(PI*kappa), "Please increase `a` or decrease `rho`!"
+        posis = np.random.exponential(scale=1/kappa, size=N) % (2*PI)
+        dists = geodesic_distances(posis)
+        simis = overlap(dists, a)/(2*PI*a)
+        probs = (simis*slope(rho, kappa, a)).clip(0,1)
+
+    if return_positions:
+        return posis, probs
+    else:
+        return probs
 
 
-def network_model(N, rho, kappa, a=0.5):
+def network_model(N, rho, beta, a=0.5, return_positions=False):
     """
     Network model as described in [1]. The output is the (empirical) positions
     of the nodes placed on the circle according to a von Mises distribution,
     followed by a tripple consisting of the (empirical) distribution of the
     pairwise distances, similarities and connection probabilities respectively.
     """
-    probs = weighted_network_model(N = N,
-                                   rho = rho,
-                                   kappa = kappa,
-                                   a = a)
-    rands = np.random.uniform(size=round(N*(N-1)/2))
+    if return_positions:
+        posis, probs = weighted_network_model(N = N,
+                                              rho = rho,
+                                              beta = beta,
+                                              a = a,
+                                              return_positions = True)
+    else:
+        probs = weighted_network_model(N = N,
+                                       rho = rho,
+                                       beta = beta,
+                                       a = a,
+                                       return_positions = False)
+
+    rands = np.random.uniform(size=int(N*(N-1)/2))
     A = squareform(np.where(probs>rands, 1, 0))
     G = nx.from_numpy_array(A)
-    return G
+
+    if return_positions:
+        return posis, G
+    else:
+        return G
 
 # =============================================================================
 #  ------------------------------ REFERENCES ---------------------------------
