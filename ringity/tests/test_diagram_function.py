7b39f909ef824197b6a9ea8370f827767d8a9303
kiri93 <mk.youssef@hotmail.com>
2022-01-20 01:57:51 +0100

    integrated numba-based implementation of net_flow but tests still don't run

--- ringity/tests/test_diagram_function.py
+++ ringity/tests/test_diagram_function.py
@@ -16,6 +16,24 @@ class TestDiagramFunction(unittest.TestCase):
         for (pt1,pt2) in zip(dgm1, dgm2):
             self.assertAlmostEqual(pt1.birth,pt2.birth, places=5)
             self.assertAlmostEqual(pt1.death,pt2.death, places=5)
+            
+    def test_lipid_network_without_self_loops(self):
+        G = nx.read_edgelist(f"{RINGITY_PATH}/test_data/lipid_coexp_network.txt")
+        G = nx.convert_node_labels_to_integers(G)
+        dgm1 = ringity.core.diagram(G)
+        dgm2 = ringity.classes.diagram.load_dgm(f"{RINGITY_PATH}/test_data/lipid_coexp_dgm.txt")
+        for (pt1,pt2) in zip(dgm1, dgm2):
+            self.assertAlmostEqual(pt1.birth,pt2.birth, places=5)
+            self.assertAlmostEqual(pt1.death,pt2.death, places=5)
+            
+    def test_lipid_network_with_integer_labels(self):
+        G = nx.read_edgelist(f"{RINGITY_PATH}/test_data/lipid_coexp_network.txt")
+        G.remove_edges_from(nx.selfloop_edges(G))
+        dgm1 = ringity.core.diagram(G)
+        dgm2 = ringity.classes.diagram.load_dgm(f"{RINGITY_PATH}/test_data/lipid_coexp_dgm.txt")
+        for (pt1,pt2) in zip(dgm1, dgm2):
+            self.assertAlmostEqual(pt1.birth,pt2.birth, places=5)
+            self.assertAlmostEqual(pt1.death,pt2.death, places=5)
 
     def test_pathological_cases(self):
         pass
