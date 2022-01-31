9ab479e930d30b454975917314c08122a4480f07
kiri93 <mk.youssef@hotmail.com>
2022-01-31 01:26:12 +0100

    generate_positions, calculate_distances and calculate_similarities are working now. Testing is still needed!

--- ringity/tests/tmp_code_snippets.py
+++ ringity/tests/tmp_code_snippets.py
@@ -5,3 +5,7 @@ rho = 0.1
 delay = 0.123
 
 network_builder = NetworkBuilder(N, delay=delay, rate=None, rho=rho, a=None, k_max=None)
+network_builder.generate_positions()
+network_builder.calculate_distances()
+network_builder.calculate_similarities()
+network_builder.calculate_probabilities()
