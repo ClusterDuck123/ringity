267f1418642631f870df5f23684442f211490b9c
kiri93 <mk.youssef@hotmail.com>
2022-02-14 11:39:54 +0100

    Started with position distribtion in walking_guide_network_model.ipynb

--- ringity/tests/tmp_code_snippets_network.py
+++ ringity/tests/tmp_code_snippets_network.py
@@ -1,11 +1,35 @@
-from ringity.classes.network_model import NetworkBuilder
+from ringity.classes.network_model import NetworkBuilder, PeriodicPositionGenerator
+from ringity.classes._distns import get_frozen_wrappedexpon
 
 N = 2**5
 rho = 0.1
 delay = 0.123
 
-network_builder = NetworkBuilder(N, delay=delay, rate=None, rho=rho, a=None, k_max=None)
-network_builder.generate_positions()
-network_builder.calculate_distances()
-network_builder.calculate_similarities()
-network_builder.calculate_probabilities()
+a = None
+rate = None
+k_max = None
+
+def ringity_model(N, density_arg, distn_arg,
+                  distn_argtype = 'delay',
+                  density_argtype = 'density',
+                  a = None, 
+                  random_state = None):
+    
+    frozen_distn = get_frozen_wrappedexpon(arg = distn_arg, 
+                                           argtype = distn_argtype)
+    periodic_positions = PeriodicPositionGenerator(N, 
+                                                   distn_arg = frozen_distn, 
+                                                   random_state = random_state)
+    
+    network = NetworkBuilder(N, periodic_positions)
+    network.generate_positions()
+    network.calculate_distances()
+    network.calculate_similarities()
+    network.calculate_probabilities()
+    network.generate_network()
+
+    return network.network()
+    
+ringity_model(N=N, a=a, 
+              density_arg = rho, density_argtype = 'density', 
+              distn_arg = delay, distn_argtype = 'delay')
