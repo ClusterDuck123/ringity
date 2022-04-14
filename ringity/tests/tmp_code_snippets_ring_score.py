b0a7e57a416dd73dfc1e4baabdce24f75a378e1f
kiri93 <mk.youssef@hotmail.com>
2022-04-15 01:52:22 +0200

    moved notebooks to doc/ folder; finished geometric and linear ring score

--- ringity/tests/tmp_code_snippets_ring_score.py
+++ ringity/tests/tmp_code_snippets_ring_score.py
@@ -1,28 +1,37 @@
-import scanpy
+import numpy as np
 import ringity as rng
-import random
 
-# breast cancer data set
-adata1 = scanpy.datasets.ebi_expression_atlas("E-GEOD-75688")
+seq1 = sorted(np.random.uniform(size = 10), reverse=True)
+#seq1 = (1,0,0,0,0)
+seq2 = seq1[:3]
 
-# melanoma data set
-adata2 = scanpy.datasets.ebi_expression_atlas("E-GEOD-81383")
+print(seq2)
 
-cc_genes = rng.get_cell_cycle_genes(gene_id='ensembl')
+random_base1 = np.random.uniform(0,10)
+random_base2 = np.random.uniform(0,10)
 
-rand_genes1 = random.sample(list(adata1.var_names),len(cc_genes))
-rand_genes2 = random.sample(list(adata2.var_names),len(cc_genes))
+ring_score = rng.linear_ring_score
 
-cc_ring_score1 = rng.ring_score(adata1[:,cc_genes].X)
-rand_ring_score1 = rng.ring_score(adata1[:,rand_genes1].X)
-
-cc_ring_score2 = rng.ring_score(adata2[:,cc_genes].X)
-rand_ring_score2 = rng.ring_score(adata2[:,rand_genes2].X)
-
-print(f"Ring-score of breast cancer cell-cycle genes: {cc_ring_score1:.3f}")
-print(f"Ring-score of breast cancer random genes    : {rand_ring_score1:.3f}")
+print(rng.geometric_ring_score(seq1))
+print(rng.geometric_ring_score(seq2))
 
 print()
 
-print(f"Ring-score of melanoma cell-cycle genes: {cc_ring_score2:.3f}")
-print(f"Ring-score of melanoma random genes    : {rand_ring_score2:.3f}")
+print(ring_score(seq1, base = random_base1, nb_pers = 2))
+print(ring_score(seq2, base = random_base1, nb_pers = 2))
+print(ring_score(seq1, base = random_base2, nb_pers = 2))
+print(ring_score(seq2, base = random_base2, nb_pers = 2))
+
+print()
+
+print(ring_score(seq1, base = random_base1, nb_pers = 3))
+print(ring_score(seq2, base = random_base1, nb_pers = 3))
+print(ring_score(seq1, base = random_base2, nb_pers = 3))
+print(ring_score(seq2, base = random_base2, nb_pers = 3))
+
+print()
+
+print(ring_score(seq1, base = random_base1, nb_pers = 4))
+print(ring_score(seq2, base = random_base1, nb_pers = 4))
+print(ring_score(seq1, base = random_base2, nb_pers = 4))
+print(ring_score(seq2, base = random_base2, nb_pers = 4))
