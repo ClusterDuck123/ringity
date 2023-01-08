87b2ddac9b58a1b164abd20ba3cc93e617742197
kiri93 <mk.youssef@hotmail.com>
2023-01-08 19:19:11 +0100

    refactored some code in geometric_networks.py

--- ringity/generators/geometric_networks.py
+++ ringity/generators/geometric_networks.py
@@ -12,19 +12,44 @@ def circle(N,
         rel_dist_th = None,
         abs_dist_th = None,
         density_th = None,
+        seed = None,
         return_point_cloud = False,
-        seed = None):
-    """Construct geometric network from uniformly sampled annulus."""
+        keep_weights = False,
+        new_weight_name = 'distance'):
+    """Construct geometric network from uniformly sampled circle."""
     X = point_clouds.circle(N = N, noise = noise, seed = seed)
-    Dsq = pdist(X)
+    G = from_point_cloud(X, 
+                er_fc_th = er_fc_th,
+                rel_dist_th = rel_dist_th,
+                abs_dist_th = abs_dist_th,
+                density_th = density_th,
+                keep_weights = keep_weights,
+                new_weight_name = new_weight_name)
+    if return_point_cloud:
+        return G, X
+    else:
+        return G
 
-    d_th = _get_threshold(Dsq, 
-                    er_fc_th = er_fc_th, 
-                    rel_dist_th = rel_dist_th, 
-                    abs_dist_th = abs_dist_th,
-                    density_th = density_th)
-    G = from_point_cloud(X, d_th)
-    
+def vonmises_circles(N, kappa, 
+        noise = 0,
+        er_fc_th = 10,
+        rel_dist_th = None,
+        abs_dist_th = None,
+        density_th = None,
+        seed = None,
+        return_point_cloud = False,
+        keep_weights = False,
+        new_weight_name = 'distance'):
+    """Construct geometric network from von Mises distributed 
+    the of circle."""
+    X = point_clouds.vonmises_circles(N = N, kappa = kappa, noise = noise, seed = seed)
+    G = from_point_cloud(X, 
+                er_fc_th = er_fc_th,
+                rel_dist_th = rel_dist_th,
+                abs_dist_th = abs_dist_th,
+                density_th = density_th,
+                keep_weights = keep_weights,
+                new_weight_name = new_weight_name)
     if return_point_cloud:
         return G, X
     else:
@@ -32,64 +57,106 @@ def circle(N,
 
 
 def annulus(N, r, 
-        abs_th = None,
-        rel_th = 0.25,
         noise = 0,
+        er_fc_th = 10,
+        rel_dist_th = None,
+        abs_dist_th = None,
+        density_th = None,
+        seed = None,
         return_point_cloud = False,
-        seed = None):
-    """Construct geometric network from uniformly sampled annulus."""
+        keep_weights = False,
+        new_weight_name = 'distance'):
+    """Construct geometric network from unifomly sampled annulus."""
     X = point_clouds.annulus(N = N, r = r, noise = noise, seed = seed)
-
-    d_th = _get_threshold(rel_th, abs_th, X)
-    G = from_point_cloud(X, d_th)
-    
+    G = from_point_cloud(X, 
+                er_fc_th = er_fc_th,
+                rel_dist_th = rel_dist_th,
+                abs_dist_th = abs_dist_th,
+                density_th = density_th,
+                keep_weights = keep_weights,
+                new_weight_name = new_weight_name)
     if return_point_cloud:
         return G, X
     else:
         return G
 
 def cylinder(N, height,
-        abs_th = None,
-        rel_th = 0.25,
         noise = 0,
+        er_fc_th = 10,
+        rel_dist_th = None,
+        abs_dist_th = None,
+        density_th = None,
+        seed = None,
         return_point_cloud = False,
-        seed = None):
-    """Construct geometric network from uniformly sampled cylinder."""
+        keep_weights = False,
+        new_weight_name = 'distance'):
+    """Construct geometric network from unifomly sampled cylinder."""
     X = point_clouds.cylinder(N = N, height = height, noise = noise, seed = seed)
+    G = from_point_cloud(X, 
+                er_fc_th = er_fc_th,
+                rel_dist_th = rel_dist_th,
+                abs_dist_th = abs_dist_th,
+                density_th = density_th,
+                keep_weights = keep_weights,
+                new_weight_name = new_weight_name)
+    if return_point_cloud:
+        return G, X
+    else:
+        return G
 
-    d_th = _get_threshold(rel_th, abs_th, X)
-    G = from_point_cloud(X, d_th)
-    
+def torus(N, r,
+        noise = 0,
+        er_fc_th = 10,
+        rel_dist_th = None,
+        abs_dist_th = None,
+        density_th = None,
+        seed = None,
+        return_point_cloud = False,
+        keep_weights = False,
+        new_weight_name = 'distance'):
+    """Construct geometric network from unifomly sampled torus."""
+    X = point_clouds.torus(N = N, r = r, noise = noise, seed = seed)
+    G = from_point_cloud(X, 
+                er_fc_th = er_fc_th,
+                rel_dist_th = rel_dist_th,
+                abs_dist_th = abs_dist_th,
+                density_th = density_th,
+                keep_weights = keep_weights,
+                new_weight_name = new_weight_name)
     if return_point_cloud:
         return G, X
     else:
         return G
 
 
-def from_point_cloud(X, dist_th, 
+def from_point_cloud(X,
+                er_fc_th = 10,
+                rel_dist_th = None,
+                abs_dist_th = None,
+                density_th = None,
                 keep_weights = False,
                 new_weight_name = 'distance'):
     """Construct geometric network from point cloud."""
 
-    D = squareform(pdist(X))
+    Dsq = pdist(X)
+    D = squareform(Dsq)
+
+    d_th = _get_threshold(Dsq, 
+                    er_fc_th = er_fc_th, 
+                    rel_dist_th = rel_dist_th, 
+                    abs_dist_th = abs_dist_th,
+                    density_th = density_th)
+
     if keep_weights:
-        A = coo.coo_matrix(np.where(D > dist_th, 0, D))
+        A = coo.coo_matrix(np.where(D > d_th, 0, D))
         G = nx.from_scipy_sparse_matrix(A, edge_attribute = new_weight_name)
     else:
-        A = np.where(D > dist_th, 0, 1)
+        A = np.where(D > d_th, 0, 1)
         np.fill_diagonal(A, 0)
         G = nx.from_numpy_array(A)
         for (a, b, d) in G.edges(data = True):
             d.clear()
     return G
-
-def from_distance_matrix(D, 
-                    density = 0.05,
-                    abs_th = None,
-                    rel_th = None,
-                    keep_weights = False,
-                    new_weight_name = 'distance'):
-    pass
 
 def _get_threshold(Dsq, rel_dist_th, abs_dist_th, density_th, er_fc_th):
     non_fc_th_types = [
