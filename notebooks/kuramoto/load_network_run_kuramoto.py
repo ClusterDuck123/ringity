16103b960b3df10e17fa1184f092386f386d9192
SeasonsOfTheSun <joel@lbi-netmed.com>
2025-04-11 09:32:47 +0200

    added command line arguments for the running step

--- notebooks/kuramoto/load_network_run_kuramoto.py
+++ notebooks/kuramoto/load_network_run_kuramoto.py
@@ -8,11 +8,11 @@ import tqdm
 import uuid
 import os
 
-def run_and_save(network_folder):
+def run_and_save(network_folder,T,dt):
     
     graph_obj = MyModInstance.load_instance(network_folder)
     
-    run = graph_obj.run()
+    run = graph_obj.run(T=T,dt=dt)
     
 
   
@@ -24,11 +24,16 @@ def main():
     """Main function to run batch simulations of Kuramoto models on loaded networks."""
     parser = argparse.ArgumentParser(description="Run Kuramoto simulation.")
     parser.add_argument("--i", type=str, default="test_network", help="The output folder")
+    parser.add_argument("--T", type=float, default=1000, help="Time to run the system for (not the number of timesteps! that is floor(T/dt))")
+    parser.add_argument("--dt", type=float, default=0.001, help="time interval per step")
+
+
     args = parser.parse_args()
     
     input_folder  = args.i
+
     for subfolder in tqdm.tqdm(os.listdir(input_folder)):
-        run_and_save(os.path.join(input_folder, subfolder))
+        run_and_save(os.path.join(input_folder, subfolder),args.T,args.dt)
 
 
 
