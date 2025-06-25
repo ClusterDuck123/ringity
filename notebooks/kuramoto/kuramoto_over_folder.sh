801cb846f6fcfc90ce290ff018029d5b19300381
SeasonsOfTheSun <joel@lbi-netmed.com>
2025-06-25 10:28:44 +0200

    update

--- notebooks/kuramoto/kuramoto_over_folder.sh
+++ notebooks/kuramoto/kuramoto_over_folder.sh
@@ -10,7 +10,7 @@ N_RUNS_PER_NETWORK=1
 for i in $(seq 1 $N_RUNS_PER_NETWORK);
 do
    for network_folder in $(ls data/parameter_array/);
-    do echo iteraction $i of  "python run_kuramoto.py --i data/parameter_array/$network_folder --summary-info-file data/parameter_array_summary.csv --only-summary True"
+    do echo iteration $i of  "python run_kuramoto.py --i data/parameter_array/$network_folder --summary-info-file data/parameter_array_summary.csv --only-summary True"
     python run_kuramoto.py --i data/parameter_array/$network_folder --summary-info-file data/parameter_array_summary.csv --only-summary True; 
     done;
 done
